3cff38c1b288c58b30c27c4964fea7620f59cece
wuchun <554503527@qq.com>
2022-01-02 00:29:41 +0800

    完成通讯，缺少UI

--- 服务器.py
+++ 服务器.py
@@ -1,43 +1,60 @@
 import socket
+from threading import Thread, Lock
 
-flag = 0
 
-while True:
-    # 创建一个socket套接字，该套接字还没有建立连接
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    # 绑定监听端口，这里必须填本机的IP192.168.27.238，localhost和127.0.0.1是本机之间的进程通信使用的
-    server.bind(('172.19.85.143', 9999))
-    # 开始监听，并设置最大连接数
-    server.listen(5)
-
-    print('waiting for connect...')
-    # 等待连接，一旦有客户端连接后，返回一个建立了连接后的套接字和连接的客户端的IP和端口元组
-    connect, (host, port) = server.accept()
-    connect.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print('the client ["{}":{}] has connected.'.format(host, port))
-
+def communicate(connect, host, port):
+    index = 0
     while True:
         try:
             # 接受客户端的数据
             data = connect.recv(1024)
             # 如果接受到客户端要quit就结束循环
-            if data == b'final_quit':
-                flag = 1
-                break
             if data == b'quit' or data == b'':
-                print('the client has quit.')
+                print('["{}":{}] 断开连接'.format(host, port))
+                connect.sendall(data)
                 break
             else:
                 # 发送数据给客户端
-                connect.sendall(("消息收到啦，你说了：" + data.decode('utf-8')).encode('utf-8'))
-                print('the client say:' + data.decode('utf-8'))
+                clientLock.acquire()  # 进锁
+                for client in socket_list:
+                    client.sendall(("[{}:{}] :".format(host, port) + data.decode('utf-8')).encode('utf-8'))
+                clientLock.release()  # 释放
+                # socket_list[0].sendall('["{}":{}] :'.format(host, port) + data.decode('utf-8')).encode('utf-8')
+                # connect.sendall(("我 ：" + data.decode('utf-8')).encode('utf-8'))
+                print('["{}":{}] :'.format(host, port) + data.decode('utf-8'))
         except:
-            print("发生错误，大概率用户强行断开了链接!")
+            print('["{}":{}] 用户强行断开了链接!'.format(host, port))
             break
     # 结束socket
-    connect.shutdown(2)
+    # connect.shutdown(2)
+    clientLock.acquire()  # 进锁
+    for i in range(0, len(add_list)):
+        if add_list[i] == ('["{}":{}] :'.format(host, port)):
+            add_list.pop(i)
+            socket_list.pop(i)
+            break
+    clientLock.release()  # 释放
+
     connect.close()
-    server.close()
-    if flag == 1:
-        break
+
+
+if __name__ == "__main__":
+    clientLock = Lock()  # 创建用户列表管理锁
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('172.19.85.143', 9999))
+    server.listen(5)
+    add_list = []
+    socket_list = []
+    while True:
+        print('\nwaiting for connect...')
+        connect1, (host1, port1) = server.accept()
+        connect1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        print('the client ["{}":{}] has connected.'.format(host1, port1))
+        add_list.append('["{}":{}] :'.format(host1, port1))  # 将地址加入列表中
+        socket_list.append(connect1)  # 将socket对象加入列表中
+
+        client_thread = Thread(target=communicate, args=(connect1, host1, port1))  # 创建一个新的子线程
+        client_thread.start()
+
--- 本地.py
+++ 本地.py
@@ -1,12 +1,27 @@
 import socket
+from threading import Thread, Lock
 
+flag = 0
+flagLock = Lock()
+
+
+def receive():
+    while True:
+        rec_data = client.recv(1024)
+        if flag == 1:
+            break
+        print(rec_data.decode('utf-8'))
+
+
+print('退出请输入"quit"')
 # 创建一个socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 主动去连接局域网内IP为192.168.27.238，端口为6688的进程
 ip = socket.gethostbyname(socket.gethostname())  # 获取自身私网ip
-# print(ip)
-# client.bind((ip, 9996))
 client.connect(('114.55.245.228', 9999))
+
+thread = Thread(target=receive)
+thread.start()
 
 while True:
     # 接受控制台的输入
@@ -16,15 +31,15 @@ while True:
     # 如果输入quit则退出连接
     if data == b'quit' or data == b'final_quit':
         print('connect quit.')
+        flag = 1
         break
     else:
         # 发送数据
         client.sendall(data)
         # 接收服务端的反馈数据
-        rec_data = client.recv(1024)
-        print(rec_data.decode('utf-8'))
 
 # 发送数据告诉服务器退出连接
 client.sendall(b'quit')
-client.shutdown(2)
+thread.join()
+# client.shutdown(2)
 client.close()
